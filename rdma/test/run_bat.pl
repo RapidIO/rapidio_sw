780ce71451e8af7c99a469632752ae30b58d25c0
Sherif Abdel-kader <career@isherif.com>
2015-06-11 21:23:39 -0400

    Run BAT tests in sequence then randomly. Disable debug messages.

--- rdma/test/run_bat.pl
+++ rdma/test/run_bat.pl
@@ -6,15 +6,37 @@
 # 2. On a second computer run this script
 
 #Open log file
+my @tests = (
+	"sudo ./bat_client -d1 -c9 -ta -obat.log",	# 0
+	"sudo ./bat_client -d1 -c9 -tb -obat.log",	# 1
+	"sudo ./bat_client -d1 -c9 -tc -obat.log",	# 2
+	"sudo ./bat_client -d1 -c9 -tg -obat.log",	# 3
+	"sudo ./bat_client -d1 -c9 -th -obat.log",	# 4
+	"sudo ./bat_client -d1 -c9 -ti -obat.log",	# 5
+	"sudo ./bat_client -d1 -c9 -t1 -obat.log",	# 6
+	"sudo ./bat_client -d1 -c9 -t2 -obat.log",	# 7
+	"sudo ./bat_client -d1 -c9 -t3 -obat.log",	# 8
+	"sudo ./bat_client -d1 -c9 -t4 -obat.log",	# 9
+	"sudo ./bat_client -d1 -c9 -t5 -obat.log",	# 10
+);
+
+my $num_bat = scalar@tests;
+print "There are $num_bat tests to run\n";
+
+# Open log file for storing PASS/FAIL results of tests
 open(my $fh, ">", "bat.log")
 	or die "cannot open > bat.log $!";
 
-system("sudo ./bat_client -d1 -c9 -ta -obat.log");
-system("sudo ./bat_client -d1 -c9 -tb -obat.log");
-system("sudo ./bat_client -d1 -c9 -tc -obat.log");
-system("sudo ./bat_client -d1 -c9 -t1 -obat.log");
-system("sudo ./bat_client -d1 -c9 -t2 -obat.log");
-system("sudo ./bat_client -d1 -c9 -t3 -obat.log");
-system("sudo ./bat_client -d1 -c9 -t4 -obat.log");
-system("sudo ./bat_client -d1 -c9 -t5 -obat.log");
+# Run tests, in sequence
+foreach my $bat(@tests) {
+	print "Running $bat at " . localtime() ."\n";
+	system($bat);
+}
+
+# Run tests, in a random order
+for (my $i = 0; $i < $num_bat; $i++) {
+	my $j = int(rand($num_bat));
+	print "Running $tests[$j] at " . localtime() ."\n";
+	system($tests[$j]);
+}
 close($fh);
