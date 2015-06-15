52a9a82a82c32ac88522c91c92de1eeadd1f5eb2
Sherif Abdel-kader <career@isherif.com>
2015-06-15 15:12:39 -0400

    Update test script to take channel & destid
    Also checks that 'rpcbind' is running.

--- rdma/test/run_bat.pl
+++ rdma/test/run_bat.pl
@@ -1,23 +1,65 @@
 #!/usr/bin/perl
 
+use warnings;
+use strict;
+
+use Getopt::Std;
+
 # Script for running Batch Automated Tests (BATs).
 #
 # 1. On one computer run 'bat_server'
 # 2. On a second computer run this script
 
-#Open log file
+# Make sure 'rpcbind' is running
+my @rpcbind = `ps -fewa | grep rpcbind`;
+if (!scalar @rpcbind) {
+	print "ERROR: 'rpcbind' is not running. Please start and try again\n";
+	exit;
+}
+
+# Command-line options
+my %options = ();
+getopts("c:d:h", \%options);
+
+if (defined($options{h})) {
+	print "run_bat.pl -h -d<destid> -c<channel\n";
+	print "-h	Displays this help message\n";
+	print "-d	Destination ID running bat_server\n";
+	print "-c	Channel for test signalling with bat_server\n";
+	exit;
+}
+
+# Channel
+my $channel;
+if (!defined($options{c})) {
+	print "ERROR: Channel not specified. Use -c<channel>\n";
+	exit;
+} else {
+	$channel = $options{c};
+}
+
+# Destination ID
+my $destid;
+if (!defined($options{d})) {
+	print "ERROR: Destination not specified. Use -d<destid>\n";
+	exit;
+} else {
+	$destid = $options{d};
+}
+
+# List of tests
 my @tests = (
-	"sudo ./bat_client -d1 -c9 -ta -obat.log",	# 0
-	"sudo ./bat_client -d1 -c9 -tb -obat.log",	# 1
-	"sudo ./bat_client -d1 -c9 -tc -obat.log",	# 2
-	"sudo ./bat_client -d1 -c9 -tg -obat.log",	# 3
-	"sudo ./bat_client -d1 -c9 -th -obat.log",	# 4
-	"sudo ./bat_client -d1 -c9 -ti -obat.log",	# 5
-	"sudo ./bat_client -d1 -c9 -t1 -obat.log",	# 6
-	"sudo ./bat_client -d1 -c9 -t2 -obat.log",	# 7
-	"sudo ./bat_client -d1 -c9 -t3 -obat.log",	# 8
-	"sudo ./bat_client -d1 -c9 -t4 -obat.log",	# 9
-	"sudo ./bat_client -d1 -c9 -t5 -obat.log",	# 10
+	"sudo ./bat_client -d" . $destid. " -c" . $channel . " -ta -obat.log",	# 0
+	"sudo ./bat_client -d" . $destid. " -c" . $channel . " -tb -obat.log",	# 1
+	"sudo ./bat_client -d" . $destid. " -c" . $channel . " -tc -obat.log",	# 2
+	"sudo ./bat_client -d" . $destid. " -c" . $channel . " -tg -obat.log",	# 3
+	"sudo ./bat_client -d" . $destid. " -c" . $channel . " -th -obat.log",	# 4
+	"sudo ./bat_client -d" . $destid. " -c" . $channel . " -ti -obat.log",	# 5
+	"sudo ./bat_client -d" . $destid. " -c" . $channel . " -t1 -obat.log",	# 6
+	"sudo ./bat_client -d" . $destid. " -c" . $channel . " -t2 -obat.log",	# 7
+	"sudo ./bat_client -d" . $destid. " -c" . $channel . " -t3 -obat.log",	# 8
+	"sudo ./bat_client -d" . $destid. " -c" . $channel . " -t4 -obat.log",	# 9
+	"sudo ./bat_client -d" . $destid. " -c" . $channel . " -t5 -obat.log",	# 10
 );
 
 my $num_bat = scalar@tests;
@@ -34,7 +76,7 @@ foreach my $bat(@tests) {
 }
 
 # Run tests, in a random order
-for (my $i = 0; $i < $num_bat; $i++) {
+for (my $i = 0; $i < $num_bat*10; $i++) {
 	my $j = int(rand($num_bat));
 	print "Running $tests[$j] at " . localtime() ."\n";
 	system($tests[$j]);
