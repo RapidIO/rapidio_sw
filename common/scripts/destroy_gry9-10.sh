7c289e0962bfd81e3458d2598e45eed06eeea15c
Sherif Abdel-kader <sherif@sakdev.com>
2015-10-02 16:24:31 -0400

    Increase iterations to 1000

---
+++ common/scripts/destroy_gry9-10.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Temporary script to speed up RSKT test apps development.
+
+RDMA_ROOT_PATH=/home/srio/git/rapidio_sw
+RIO_CLASS_MPORT_DIR=/sys/class/rio_mport/rio_mport0
+
+NODES="GRY10 GRY9"
+
+for node in $NODES
+do
+	# Kill RSKTD
+	THE_PID=$(ssh root@"$node" pgrep rsktd)
+	echo "Killing rsktd on $node RSKTD PID=$THE_PID"
+	for proc in $THE_PID
+	do
+		ssh root@"$node" "kill -s 2 $proc"
+	done
+
+	# Kill RDMAD
+	THE_PID=$(ssh root@"$node" pgrep rdmad)
+	echo "Killing rdmad on $node RDMAD PID=$THE_PID"
+	for proc in $THE_PID
+	do
+		ssh root@"$node" "kill -s 2 $proc"
+	done
+
+	# Kill FMD
+	THE_PID=$(ssh root@"$node" pgrep fmd)
+	echo "Killing fmd on $node RDMAD PID=$THE_PID"
+	for proc in $THE_PID
+	do
+		ssh root@"$node" "kill -s 2 $proc"
+	done
+
+	# Unload rio_cm
+	ssh root@"$node" "modprobe -r rio_cm"
+	sleep 1
+
+	# Load rio_cm
+	ssh root@"$node" "modprobe rio_cm"
+done
+
