a1636a67c4eecb66ef819f506846be4745066a7f
Sherif Abdel-kader <career@isherif.com>
2015-10-09 12:49:35 -0400

    Add missing header and 'using' for 'find'

--- common/include/ts_vector.h
+++ common/include/ts_vector.h
@@ -3,11 +3,13 @@
 
 #include <pthread.h>
 
+#include <algorithm>
 #include <vector>
 #include <string>
 
 using std::vector;
 using std::string;
+using std::find;
 
 /* A thread-safe vector */
 template <typename T>
