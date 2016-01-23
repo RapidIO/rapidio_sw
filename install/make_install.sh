a3522df1522600605d04ba000a1c80395addb5ae
Barry Wood <barry.wood@idt.com>
2016-01-23 12:08:15 -0500

    Addition of standard installation files for 4 node cluster.

---
+++ install/make_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd $1
+git checkout idt/release03_rdma
+make -s clean
+make -s all
+cd utils/goodput
+make -s clean
+make -s all
+doxygen doxyconfig
+cd ../file_transfer
+make -s clean
+make -s all
+cd ../..
+doxygen doxyconfig
+git status
+cd ..
+chgrp -R $2 rapidio_sw
