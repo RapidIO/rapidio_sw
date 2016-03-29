5435681e74ee0a079701a434f8acbe1843a0b611
Barry Wood <barry.wood@idt.com>
2016-03-29 16:28:25 -0400

    Fix and enhance install.sh script to correctly check for number of parameters and memory space size. Correct node2.conf configuration file to make component tag value consistent with deviceID value. Tested on Gry13 & 14.

--- install.sh
+++ install.sh
@@ -5,14 +5,38 @@ SOURCE_PATH="/home/barryw/fmd/rapidio_sw"
 CONFIG_PATH="/etc/rapidio"
 SCRIPTS_PATH=$SOURCE_PATH"/install"
 
+PRINTHELP=0
+
+if [ "$#" -lt 6 ]; then
+	echo $'\ninstall.sh requires 6 parameters.\n'
+	PRINTHELP=1
+elif [ $5 != 'mem34' -a $5 != 'mem50' -a $5 != 'mem66' ] ; then
+	echo $'\nmemsz parameter must be mem34, mem50, or mem66.\n'
+	PRINTHELP=1
+fi
+
+if [ $PRINTHELP = 1 ] ; then
+    echo "install.sh <NODE1> <NODE2> <NODE3> <NODE4> <memsz> <group> <rel>"
+    echo "<NODE1> Name of master, enumerating node"
+    echo "<NODE2> Name of slave node conncected to Switch Port 2"
+    echo "<NODE3> Name of slave node conncected to Switch Port 3"
+    echo "<NODE4> Name of slave node conncected to Switch Port 4"
+    echo "If any of <NODE2> <NODE3> <NODE4> is \"none\", the node is ignored."
+    echo "<memsz> RapidIO memory size, one of mem34, mem50, mem66"
+    echo "        If any node has more than 8 GB of memory, MUST use mem50"
+    echo "<group> Unix file ownership group which should have access to"
+    echo "        the RapidIO software"
+    echo "<rel> is the software release/version to install."
+    echo "        If no release is supplied, the current release is installed."
+    exit
+fi
+
 MASTER=$1
 SLAVES=( )
 ALLNODES=( $1 )
 NODE2=""
 NODE3=""
 NODE4=""
-
-echo "ALLNODES = " ${ALLNODES[*]}
 
 if [ $2 != 'none' ]; then
 	NODE2=$2
@@ -36,22 +60,6 @@ MEMSZ=$5
 GRP=$6
 REL=$7
 
-if [ "$#" -lt 6 -o $MEMSZ != 'mem34' -a $MEMSZ != 'mem50' -a $MEMSZ != 'mem66' ] ; then
-    echo "install.sh <NODE1> <NODE2> <NODE3> <NODE4> <memsz> <group> <rel>"
-    echo "<NODE1> Name of master, enumerating node"
-    echo "<NODE2> Name of slave node conncected to Switch Port 2"
-    echo "<NODE3> Name of slave node conncected to Switch Port 3"
-    echo "<NODE4> Name of slave node conncected to Switch Port 4"
-    echo "If any of <NODE2> <NODE3> <NODE4> is \"none\", the node is ignored."
-    echo "<memsz> RapidIO memory size, one of mem34, mem50, mem66"
-    echo "        If any node has more than 8 GB of memory, MUST use mem50"
-    echo "<group> Unix file ownership group which should have access to"
-    echo "        the RapidIO software"
-    echo "<rel> is the software release/version to install."
-    echo "        If no release is supplied, the current release is installed."
-    exit
-fi
-
 for i in "${ALLNODES[@]}"
 do
 	ping -c 1 $i > /dev/null
@@ -66,8 +74,7 @@ done
 
 echo "Beginning installation..."
 
-# for i in "${ALLNODES[@]}"
-for i in "${NOVAR[@]}"
+for i in "${ALLNODES[@]}"
 do
 	echo $i" Compilation and documentation generation starting..."
 	if [ -z "$7" ]; then
