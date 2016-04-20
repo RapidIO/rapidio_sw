efd648ffd195703b88b156c23a2b12a579c983dd
Vlad Cotfas <unix_router@yahoo.com>
2016-04-19 22:08:40 -0400

    fxfr_client.sh accept a list of dids as part of command line -- no need to SSH to get it.

--- utils/file_transfer/fxfr_client.sh
+++ utils/file_transfer/fxfr_client.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 
-MPORT=$1;
+MPORT=$1; shift;
 
 [ -z "$MPORT" ] && { echo "Must specify MPORT!" 1>&2; exit 1; }
 
 SOURCE_PATH=/opt/rapidio/rapidio_sw
 RIO_CLASS_MPORT_DIR=/sys/class/rio_mport/rio_mport$MPORT
 
-RND=$RANDOM
-
 . /etc/rapidio/nodelist.sh
 
 MY_DESTID=$(cat $RIO_CLASS_MPORT_DIR/device/port_destid 2>/dev/null);
 
-DID_LIST='';
-NODE_LIST='';
-for node in $NODES; do
-  RIODEVS=$(ssh root@"$node" "ls /sys/bus/rapidio/devices/")
-  [ -z "$RIODEVS" ] && continue;
-  DESTID=$(ssh root@"$node" "cat $RIO_CLASS_MPORT_DIR/device/port_destid 2>/dev/null")
-  [ $MY_DESTID = $DESTID ] && continue;
+DID_LIST=$@;
+if [ -z "$DID_LIST" ]; then
+  for node in $NODES; do
+    RIODEVS=$(ssh root@"$node" "ls /sys/bus/rapidio/devices/")
+    [ -z "$RIODEVS" ] && continue;
+    DESTID=$(ssh root@"$node" "cat $RIO_CLASS_MPORT_DIR/device/port_destid 2>/dev/null")
+    [ $MY_DESTID = $DESTID ] && continue;
 
-  DID_LIST="$DID_LIST $DESTID";
-done
+    DID_LIST="$DID_LIST $DESTID";
+  done
+fi
 echo "Valid did  list: $DID_LIST";
 
 [ $(id -u) -gt 0 ] && { echo "Must run as root!" 1>&2; exit 1; }
@@ -40,6 +39,8 @@ dbg=-1
 kbuf=1
 while [ $stop_req -eq 0 ]; do
   for did in $DID_LIST; do
+     [ $MY_DESTID = $did ] && continue;
+
      decdid=$(printf "%d" $did);
     ./rftp $src /dev/null $decdid 5555 $MPORT $dbg $kbuf
   done
