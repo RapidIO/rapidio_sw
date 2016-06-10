bee03b6ddec0e76f9e7937df95fd4ec4fb24bf87
Vladimir Cotfas <unix_router@yahoo.com>
2016-06-10 12:04:31 -0400

    Changed startup ordering.

--- install/all_start.sh
+++ install/all_start.sh
@@ -3,17 +3,17 @@
 SOURCE_PATH=/opt/rapidio/rapidio_sw
 RIO_CLASS_MPORT_DIR=/sys/class/rio_mport/rio_mport0
 
+START_DMATUN=n
+START_UMDD=y
 START_RDMAD=y
 START_RSKTD=y
-START_UMDD=y
-START_DMATUN=n
 
 echo "$@" | grep -qw dmatun &&  START_DMATUN=y;
-echo "$@" | grep -q norsktd && START_RSKTD=n;
 echo "$@" | grep -q noumdd  && START_UMDD=n;
 echo "$@" | grep -q nordmad && {
   START_RDMAD=n; START_RSKTD=n
 }
+echo "$@" | grep -q norsktd && START_RSKTD=n;
 
 # Platform check for UMD-based components, start only on known architectures with PCIe & Tsi721
 arch | awk -vx=1 '/(x86_64|i[3-6]86|ppc64)/{x=0;}END{exit x;}' || { # not i686, x86_64, PPC64
@@ -56,6 +56,18 @@ if [ "$START_DMATUN" = 'y' ]; then
 	done
 fi
 
+if [ "$START_UMDD" = 'y' ]; then
+	for node in $NODES; do
+		DESTID=$(ssh root@"$node" "cat $RIO_CLASS_MPORT_DIR/device/port_destid")
+		[ "$DESTID" = '0xffffffff' ] && { echo "Node $node not enumerated skipping UMDD"; continue; }
+		echo "Start UMDd/SHM on $node destID=$DESTID"
+		ssh root@"$node" "screen -dmS umdd sh $SOURCE_PATH/umdd_tsi721/umdd.sh"
+		sleep 1
+		UMDD_PID=$(ssh root@"$node" pgrep umdd)
+		echo "$node UMDd/SHM pid=$UMDD_PID"
+	done
+fi
+
 # Start RDMAD on each node
 if [ "$START_RDMAD" = 'y' ]; then
 	for node in $NODES; do
@@ -77,17 +89,5 @@ if [ "$START_RSKTD" = 'y' ]; then
 		sleep 1
 		RSKTD_PID=$(ssh root@"$node" pgrep rsktd)
 		echo "$node rsktd pid=$RSKTD_PID"
-	done
-fi
-
-if [ "$START_UMDD" = 'y' ]; then
-	for node in $NODES; do
-		DESTID=$(ssh root@"$node" "cat $RIO_CLASS_MPORT_DIR/device/port_destid")
-		[ "$DESTID" = '0xffffffff' ] && { echo "Node $node not enumerated skipping UMDD"; continue; }
-		echo "Start UMDd/SHM on $node destID=$DESTID"
-		ssh root@"$node" "screen -dmS umdd sh $SOURCE_PATH/umdd_tsi721/umdd.sh"
-		sleep 1
-		UMDD_PID=$(ssh root@"$node" pgrep umdd)
-		echo "$node UMDd/SHM pid=$UMDD_PID"
 	done
 fi
