81fc60255c43a08db75d6751619755f1cc2a759b
Vladimir Cotfas <unix_router@yahoo.com>
2016-06-10 11:53:47 -0400

    Added DMA Tun to all start/stop scripts.

--- install/all_start.sh
+++ install/all_start.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
+
 SOURCE_PATH=/opt/rapidio/rapidio_sw
 RIO_CLASS_MPORT_DIR=/sys/class/rio_mport/rio_mport0
 
-# Temporary script to speed up RSKT test apps development.
+START_RDMAD=y
+START_RSKTD=y
+START_UMDD=y
+START_DMATUN=n
+
+echo "$@" | grep -qw dmatun &&  START_DMATUN=y;
+echo "$@" | grep -q norsktd && START_RSKTD=n;
+echo "$@" | grep -q noumdd  && START_UMDD=n;
+echo "$@" | grep -q nordmad && {
+  START_RDMAD=n; START_RSKTD=n
+}
+
+# Platform check for UMD-based components, start only on known architectures with PCIe & Tsi721
+arch | awk -vx=1 '/(x86_64|i[3-6]86|ppc64)/{x=0;}END{exit x;}' || { # not i686, x86_64, PPC64
+  START_UMDD=n;
+  START_DMATUN=n;
+}
+
+set | grep ^START_
 
 . /etc/rapidio/nodelist.sh
 
@@ -21,33 +40,50 @@ do
 	echo "$node fmd pid=$FMD_PID"
 done
 
+sleep 1; # Allow FMD to enumerate nodes
+
+# Start DMA Tun on each node. It is important to start first
+# to be able to grab from kernel a size-aligned CMA IBwin.
+if [ "$START_DMATUN" = 'y' ]; then
+	for node in $NODES; do
+		DESTID=$(ssh root@"$node" "cat $RIO_CLASS_MPORT_DIR/device/port_destid")
+		[ "$DESTID" = '0xffffffff' ] && { echo "Node $node not enumerated skipping DMA Tun"; continue; }
+		echo "Start DMA Tun on $node destID=$DESTID"
+		ssh root@"$node" "screen -dmS dmatun sh $SOURCE_PATH/utils/goodput/dmatun.sh"
+		sleep 1
+		DMATUN_PID=$(ssh root@"$node" pgrep ugoodput)
+		echo "$node DMA Tun pid=$DMATUN_PID"
+	done
+fi
+
 # Start RDMAD on each node
-for node in $NODES
-do
-	DESTID=$(ssh root@"$node" "cat $RIO_CLASS_MPORT_DIR/device/port_destid")
-	echo "Start rdmad on $node destID=$DESTID"
-	ssh root@"$node" "screen -dmS rdmad $SOURCE_PATH/rdma/rdmad -l3"
-	sleep 1
-	RDMA_PID=$(ssh root@"$node" pgrep rdmad)
-	echo "$node rdmad pid=$RDMA_PID"
-done
+if [ "$START_RDMAD" = 'y' ]; then
+	for node in $NODES; do
+		DESTID=$(ssh root@"$node" "cat $RIO_CLASS_MPORT_DIR/device/port_destid")
+		echo "Start rdmad on $node destID=$DESTID"
+		ssh root@"$node" "screen -dmS rdmad $SOURCE_PATH/rdma/rdmad -l3"
+		sleep 1
+		RDMA_PID=$(ssh root@"$node" pgrep rdmad)
+		echo "$node rdmad pid=$RDMA_PID"
+	done
+fi
 
 # Start RSKTD on each node
-for node in $NODES
-do
-	DESTID=$(ssh root@"$node" "cat $RIO_CLASS_MPORT_DIR/device/port_destid")
-	echo "Start rsktd on $node destID=$DESTID"
-	ssh root@"$node" "screen -dmS rsktd $SOURCE_PATH/rdma/rskt/daemon/rsktd -l3"
-	sleep 1
-	RSKTD_PID=$(ssh root@"$node" pgrep rsktd)
-	echo "$node rsktd pid=$RSKTD_PID"
-done
-
-if arch | awk -vx=1 '/(x86_64|i[3-6]86|ppc64)/{x=0;}END{exit x;}'; then
-	# Start UMDD on each node, only on known architectures with PCIe & Tsi721
-	for node in $NODES
-	do
+if [ "$START_RSKTD" = 'y' ]; then
+	for node in $NODES; do
 		DESTID=$(ssh root@"$node" "cat $RIO_CLASS_MPORT_DIR/device/port_destid")
+		echo "Start rsktd on $node destID=$DESTID"
+		ssh root@"$node" "screen -dmS rsktd $SOURCE_PATH/rdma/rskt/daemon/rsktd -l3"
+		sleep 1
+		RSKTD_PID=$(ssh root@"$node" pgrep rsktd)
+		echo "$node rsktd pid=$RSKTD_PID"
+	done
+fi
+
+if [ "$START_UMDD" = 'y' ]; then
+	for node in $NODES; do
+		DESTID=$(ssh root@"$node" "cat $RIO_CLASS_MPORT_DIR/device/port_destid")
+		[ "$DESTID" = '0xffffffff' ] && { echo "Node $node not enumerated skipping UMDD"; continue; }
 		echo "Start UMDd/SHM on $node destID=$DESTID"
 		ssh root@"$node" "screen -dmS umdd sh $SOURCE_PATH/umdd_tsi721/umdd.sh"
 		sleep 1
