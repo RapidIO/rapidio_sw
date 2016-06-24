f629238e27f6fccdbe503b5153ac9dc63a13ed20
Vlad Cotfas <unix_router@yahoo.com>
2016-06-24 14:59:48 -0400

    .

--- rdma/rskt/samples/test_shim_conn.sh
+++ rdma/rskt/samples/test_shim_conn.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
 ulimit -c unlimited
-#LD_PRELOAD=./rskt_shim.so exec wget -vO /dev/null http://0.0.1.5
-#LD_PRELOAD=./rskt_shim.so exec wget -O /dev/null http://ipchicken.com
-#LD_PRELOAD=./rskt_shim.so exec lynx -dump http://ipchicken.com
+#LD_PRELOAD=./rskt_shim.so exec wget -vO /dev/null http://0.0.0.6
 LD_PRELOAD=./rskt_shim.so exec lynx -dump http://0.0.0.6
-#LD_PRELOAD=./rskt_shim.so exec gdb --args thttpd -D -h 0.0.1.5 -p 8001
