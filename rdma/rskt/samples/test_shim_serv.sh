0ce0599e7e21e04c4167ddc0821e0775f338d289
Vlad Cotfas <unix_router@yahoo.com>
2016-06-30 20:07:20 -0400

    .

--- rdma/rskt/samples/test_shim_serv.sh
+++ rdma/rskt/samples/test_shim_serv.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+killall -9 thttpd
 ulimit -c unlimited
+
+export LD_DEBUG=symbols
+
 #LD_PRELOAD=./rskt_shim.so exec wget -O /dev/null http://8.8.8.8
 #LD_PRELOAD=./rskt_shim.so exec wget -O /dev/null http://ipchicken.com
 #LD_PRELOAD=./rskt_shim.so exec lynx -dump http://ipchicken.com
 #LD_PRELOAD=./rskt_shim.so exec lynx -dump http://0.0.1.5
-LD_PRELOAD=./rskt_shim.so exec gdb --args thttpd -D -h 0.0.1.5 -p 8001
+#LD_PRELOAD=./rskt_shim.so exec gdb --args thttpd -D -h 0.0.1.5 -p 8001
+#LD_PRELOAD=./rskt_shim.so exec strace -e write=3 thttpd -D -h 0.0.0.6 -p 80 -l /dev/tty
+LD_PRELOAD=./rskt_shim.so exec thttpd -D -h 0.0.0.6 -p 80 -l /dev/tty
