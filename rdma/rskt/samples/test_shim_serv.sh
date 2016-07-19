62b009a949fd8c0ffb7fd29ab48435e4abcc83af
Vladimir Cotfas <unix_router@yahoo.com>
2016-07-19 10:17:55 -0400

    Copy-merge librsvdmem from mainline.

--- rdma/rskt/samples/test_shim_serv.sh
+++ rdma/rskt/samples/test_shim_serv.sh
@@ -12,4 +12,4 @@ export LD_LIBRARY_PATH=.
 #LD_PRELOAD=./rskt_shim.so exec lynx -dump http://0.0.1.5
 #LD_PRELOAD=./rskt_shim.so exec gdb --args thttpd -D -h 0.0.1.5 -p 8001
 #LD_PRELOAD=./rskt_shim.so exec strace -e write=3 thttpd -D -h 0.0.0.6 -p 80 -l /dev/tty
-LD_PRELOAD=./rskt_shim.so exec thttpd -D -h 0.0.0.6 -p 80 -l /dev/tty
+LD_PRELOAD=./rskt_shim.so exec ./thttpd -D -h 0.0.0.6 -p 80 -l /dev/tty -d $(pwd)
