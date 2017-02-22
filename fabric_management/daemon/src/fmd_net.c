1179cb256b76da655c68840783a680f423c21364
Andrew Irvine <airvine@idt.com>
2017-02-22 14:15:41 -0500

    Sonar analysis: Dynamically allocated memory should be released Move annotation

--- fabric_management/daemon/src/fmd_net.c
+++ fabric_management/daemon/src/fmd_net.c
@@ -111,11 +111,10 @@ int fmd_traverse_network_from_pe_port(riocp_pe_handle pe, rio_port_t port_num,
 
 				no_cfg->curr_pe = curr_pe;
 				no_cfg->pnum = pnum;
-				//@sonar:off - Dynamically allocated memory should be released
 				l_push_tail(&no_cfg_list, (void *)no_cfg);
-				//@sonar:on
-
+				//@sonar:off - Dynamically allocated memory should be released
 				continue;
+				//@sonar:on
 			}
 
 			rc = riocp_pe_probe(curr_pe, pnum, &new_pe,
@@ -287,9 +286,9 @@ int fmd_traverse_network_from_pe_port(riocp_pe_handle pe, rio_port_t port_num,
 						}
 						no_cfg->curr_pe = new_pe;
 						no_cfg->pnum = pnum;
-						//@sonar:off - Dynamically allocated memory should be released
 						l_push_tail(&no_cfg_list,
 								(void *)no_cfg);
+						//@sonar:off - Dynamically allocated memory should be released
 						//@sonar:on
 					}
 				}
