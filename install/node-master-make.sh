9add462b15bf85977fdc7372b0cf0dbde68e7114
Barry Wood <barry.wood@idt.com>
2017-03-03 20:57:49 -0500

    Change installation support for StarBridge 8 port switch to allow any node to be the master for install_list.sh and for install.sh.

--- install/node-master-make.sh
+++ install/node-master-make.sh
@@ -25,11 +25,39 @@ done
 #
 echo "Installing fmd configuration..."
 HOSTL=''
+MAST_LABEL='master'
+MAST_NODE=''
 while read -r line || [[ -n "$line" ]]; do
     arr=($line)
     f=${arr[3]}
     HOSTL="$HOSTL -vH_${f^^}=${arr[1]}"
+
+# NOTE: Node list has already been checked for duplicated/missing master node entries
+    if [ ${arr[0]} = "$MAST_LABEL" ]
+    then
+        MAST_NODE=${arr[3]}
+    fi
 done < "$INSTALL_ROOT/$NODEDATA_FILE"
+
+# Figure out node number of master node
+if [ -z $MAST_NODE ]
+then
+        echo "Master node not defined, failed install.  Aborting..."
+        exit 1
+fi
+
+# Determine destination ID of the master node
+# line syntax:
+# ENDPOINT <ep_name> PORT <#> <ct> <pw> <pw> <ls> <IDLE1|2|3> <EM_ON|EM_OFF> {<dev08|dev16|dev32> <devID> <hc>} END PEND
+MAST_DESTID=$(grep -e ENDPOINT < $INSTALL_ROOT/$TMPL_FILE | grep "\b${MAST_NODE}\b" | while read ep_kw epname port_kw port_num ct pw_max pw_cfg pt_ls idle em_ctl did_sz did unused; do echo $did; done)
+
+if [ -z $MAST_DESTID ]
+then
+        echo "Master destID not found, failed install.  Aborting..."
+        exit 1
+fi
+
+HOSTL="$HOSTL -vMAST_DID=${MAST_DESTID}"
 
 awk -vM=$MEM_SIZE $HOSTL '
     /MEMSZ/{gsub(/MEMSZ/, M);}
@@ -40,7 +68,8 @@ awk -vM=$MEM_SIZE $HOSTL '
     /node4/{if(H_NODE4 != "") {gsub(/node4/, H_NODE4);} else {$0="";}}
     /node3/{if(H_NODE3 != "") {gsub(/node3/, H_NODE3);} else {$0="";}}
     /node2/{if(H_NODE2 != "") {gsub(/node2/, H_NODE2);} else {$0="";}}
-    /node1/{gsub(/node1/, H_NODE1);}
+    /node1/{if(H_NODE1 != "") {gsub(/node1/, H_NODE1);} else {$0="";}}
+    {gsub(/MAST_DID/, MAST_DID);}
     {print}' $INSTALL_ROOT/$TMPL_FILE > $CONFIG_FILE
 
 
